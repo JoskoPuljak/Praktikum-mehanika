b45e394ba3fe4b5e3141ce52dcdfa626dcef7933
JoškoPuljak <jpuljak@pmfst.hr>
2024-10-28 19:59:51 +0000

    modul radi

---
+++ grafkvadrata.py
@@ -0,0 +1,2 @@
+import linereg as linrg
+linrg.najmanji_kvadrat("podaci.csv","black","masa/g","produljenje/mm","ovisnost produljenja o masi")
---
+++ linereg.py
@@ -0,0 +1,41 @@
+import math as mt
+import csv
+import matplotlib.pyplot as plt
+def aritm_sred(lista):
+    sum=0
+    for a in lista:
+        sum=sum + a
+    sredina=sum/len(lista)
+    return(sredina)
+def najmanji_kvadrat(file,colour="red",xlabel="x os", ylabel="y os", title="x/y graf",linelegend="graf linearne regresije",scatterlegend="pravi podaci"):
+    with open (file, "r") as csv_file:
+        csv_reader=csv.reader(csv_file)
+        xy=[[float(line[0]),float(line[1])] for line in csv_reader]
+        x=[i[0] for i in xy]
+        y=[i[1] for i in xy]
+        x_mean=aritm_sred(x)
+        y_mean=aritm_sred(y)
+        product=[i*j for i,j in zip(x,y)]
+        product_mean=aritm_sred(product)
+        xsquaredlist=[i**2 for i in x]
+        xsquaredmean=aritm_sred(xsquaredlist)
+
+        koeficijent_smjera=(product_mean-(x_mean*y_mean))/(xsquaredmean-(x_mean**2))
+        odsjecak=y_mean-koeficijent_smjera*x_mean
+        kx=[koeficijent_smjera*i for i in x]
+        graf=[i + odsjecak for i in kx]
+
+        plt.plot(x,graf,color=colour)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.scatter(x,y,color=colour)
+        plt.legend([linelegend,scatterlegend])
+        plt.savefig("graf najmanjeg kvadrata.png")
+        plt.show()
+def stand_dev(lista):
+    sum=0
+    for i in lista:
+        sum+=(i-aritm_sred(lista))**2
+    standardna=mt.sqrt(sum/(len(lista)-1))
+    return(standardna)
